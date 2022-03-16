23fb053ed3b38ec336593d030f6c947dc937e7a9
valdis-v <vladislavkharkov95@gmail.com>
2022-03-16 14:12:06 +0200

    Create ImagePanel, KeywordsPanel, SearchRequestPanel

---
+++ src/main/java/component/panel/ImagePanel.java
@@ -0,0 +1,33 @@
+package component.panel;
+
+import controller.Controller;
+import exception.UnsupportedComponentException;
+import interfaces.Changeable;
+import interfaces.Readable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel implements Readable<Image>, Changeable<Image> {
+    public ImagePanel() {
+        register();
+    }
+
+    private void register() {
+        try {
+            Controller.getController().registerComponent(this);
+        } catch (UnsupportedComponentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Image getData() {
+        return null;
+    }
+
+    @Override
+    public void setData(Image data) {
+
+    }
+}
---
+++ src/main/java/component/panel/KeywordsPanel.java
@@ -0,0 +1,60 @@
+package component.panel;
+
+import controller.Controller;
+import exception.UnsupportedComponentException;
+import interfaces.Changeable;
+import interfaces.Readable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeywordsPanel extends JPanel implements Readable<String>, Changeable<String> {
+    private final List<JComponent> requests;
+
+    {
+        requests = new ArrayList<>();
+    }
+
+    public KeywordsPanel() {
+        super();
+        initialize();
+        register();
+    }
+
+    private void initialize() {
+        mockRequests();
+        setName("KEYWORDS");
+        setBackground(Color.RED);
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        for (JComponent component : requests) add(component);
+    }
+
+    private void register() {
+        try {
+            Controller.getController().registerComponent(this);
+        } catch (UnsupportedComponentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mockRequests() {
+        for (int k = 0; k < 20; k++) requests.add(new JLabel("ключевое слово №" + k + " из " + this.getName()));
+    }
+
+    @Override
+    public String getData() {
+        StringBuilder builder = new StringBuilder();
+        for (JComponent component : requests) {
+            builder.append(((JLabel) component).getText());
+            builder.append("/n");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public void setData(String data) {
+
+    }
+}
