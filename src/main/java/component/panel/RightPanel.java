fb57d602cc055c0c8e88e28e2dac0b051b9afe77
valdis-v <vladislavkharkov95@gmail.com>
2022-03-16 14:13:41 +0200

    Create RightPanel, remove SearchRequestAndKeywordPanel

---
+++ src/main/java/component/panel/RightPanel.java
@@ -0,0 +1,30 @@
+package component.panel;
+
+import boot.GUIComponentParameter;
+import util.TextFormatterForGUIComponent;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RightPanel extends JTabbedPane {
+    public RightPanel() {
+        super(JTabbedPane.LEFT);
+        setPreferredSize(GUIComponentParameter.rightPanelDimension());
+        setMaximumSize(new Dimension(GUIComponentParameter.rightPanelDimension().width, GUIComponentParameter.screenDimension.height));
+        setBackground(GUIComponentParameter.rightPanelColor);
+        initializeTabs();
+        setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    }
+
+    private void initializeTabs() {
+        JPanel keywordsPanel = new KeywordsPanel();
+        add(new JScrollPane(new KeywordsPanel()), TextFormatterForGUIComponent.getVerticalText(keywordsPanel.getName()));
+
+        JPanel searchRequestsPanel = new SearchRequestPanel();
+        add(new JScrollPane(searchRequestsPanel), TextFormatterForGUIComponent.getVerticalText(searchRequestsPanel.getName()));
+
+        JPanel imagesPanel = new ImagePanel();
+        imagesPanel.setName("IMAGES");
+        add(new JScrollPane(imagesPanel), TextFormatterForGUIComponent.getVerticalText(imagesPanel.getName()));
+    }
+}
