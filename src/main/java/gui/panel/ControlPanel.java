99f2fff9d61dd479dd445097223401d0bc59264c
valdis-v <vladislavkharkov95@gmail.com>
2022-03-16 17:19:45 +0200

    Add mock buttons for check Controller.getText() methods

--- src/main/java/gui/panel/ControlPanel.java
+++ src/main/java/gui/panel/ControlPanel.java
@@ -5,6 +5,7 @@ import controller.Controller;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ControlPanel extends JPanel {
     public ControlPanel() {
@@ -16,11 +17,28 @@ public class ControlPanel extends JPanel {
     }
 
     private void initializeButtons() {
-        for (int i = 0; i <= 6; ++i) {
-            JButton button = new JButton("button " + i);
+        ArrayList<JButton> buttonList = new ArrayList<>();
+
+        JButton getTextFromText = new JButton("getTextFromText");
+        getTextFromText.addActionListener(l -> System.out.println(Controller.getController().getCurrentText()));
+        buttonList.add(getTextFromText);
+        JButton getTextFromRequest = new JButton("getTextFromRequest");
+        getTextFromRequest.addActionListener(l -> System.out.println(Controller.getController().getCurrentRequest()));
+        buttonList.add(getTextFromRequest);
+        JButton getTextFromKeywords = new JButton("getTextFromKeywords");
+        getTextFromKeywords.addActionListener(l -> System.out.println(Controller.getController().getCurrentKeys()));
+        buttonList.add(getTextFromKeywords);
+        JButton getTextFromSearch = new JButton("getTextFromSearch");
+        getTextFromSearch.addActionListener(l -> System.out.println(Controller.getController().getCurrentSearchRequests()));
+        buttonList.add(getTextFromSearch);
+        JButton checkIsInitialised = new JButton("checkIsInitialised");
+        checkIsInitialised.addActionListener(l -> System.out.println("is initialised " + Controller.getController().isInitialised()));
+        buttonList.add(checkIsInitialised);
+
+
+        for(JButton button : buttonList){
             button.setPreferredSize(new Dimension(GUIComponentParameter.controlPanelDimension().width / 2 - 7, GUIComponentParameter.controlPanelDimension().height / 3 - 7));
             add(button);
-            button.addActionListener(l -> System.out.println(Controller.getController().isInitialised()));
         }
     }
 }
