5d1b2ed085395b0aeac43aedf47b2e54aac75bef
valdis-v <vladislavkharkov95@gmail.com>
2022-03-18 04:26:28 +0200

    Add methods for removing elements from Changeable

--- src/main/java/interfaces/Changeable.java
+++ src/main/java/interfaces/Changeable.java
@@ -1,5 +1,9 @@
 package interfaces;
 
-public interface Changeable<E> {
+public interface Changeable<E> extends Readable<E> {
     void setData(E data);
+
+    void removeLast();
+
+    void removeAll();
 }
