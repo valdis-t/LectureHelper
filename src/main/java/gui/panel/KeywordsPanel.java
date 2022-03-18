b483f74366f4a958277f339f4d5dc0f85daa3b57
valdis-v <vladislavkharkov95@gmail.com>
2022-03-18 04:44:58 +0200

    Modify Keywords/SearchRequestPanel as extends from RequestPanel

--- src/main/java/gui/panel/KeywordsPanel.java
+++ src/main/java/gui/panel/KeywordsPanel.java
@@ -1,58 +1,12 @@
 package gui.panel;
 
-import controller.Controller;
-import exception.UnsupportedComponentException;
-import interfaces.Changeable;
-import interfaces.Readable;
-
-import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class KeywordsPanel extends JPanel implements Readable<String>, Changeable<String> {
-    private final List<JComponent> requests;
-
-    {
-        requests = new ArrayList<>();
-    }
-
-    public KeywordsPanel() {
-        super();
-        initialize();
-        register();
-    }
-
-    private void initialize() {
+public class KeywordsPanel extends RequestPanel {
+    protected void initialize() {
         setName("KEYWORDS");
         setBackground(Color.RED);
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        for (JComponent component : requests) add(component);
     }
 
-    private void register() {
-        try {
-            Controller.getController().registerComponent(this);
-        } catch (UnsupportedComponentException e) {
-            e.printStackTrace();
-        }
-    }
 
-    @Override
-    public String getData() {
-        StringBuilder builder = new StringBuilder();
-        for (JComponent component : requests) {
-            builder.append(((JLabel) component).getText());
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
-    @Override
-    public void setData(String data) {
-        JLabel label = new JLabel(data);
-        requests.add(label);
-        add(label);
-        revalidate();
-    }
 }
--- src/main/java/gui/panel/SearchRequestPanel.java
+++ src/main/java/gui/panel/SearchRequestPanel.java
@@ -1,58 +1,10 @@
 package gui.panel;
 
-import controller.Controller;
-import exception.UnsupportedComponentException;
-import interfaces.Changeable;
-import interfaces.Readable;
-
-import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class SearchRequestPanel extends JPanel implements Readable<String>, Changeable<String> {
-    private final List<JComponent> requests;
-
-    {
-        requests = new ArrayList<>();
-    }
-
-    public SearchRequestPanel() {
-        super();
-        initialize();
-        register();
-    }
-
-    private void register() {
-        try {
-            Controller.getController().registerComponent(this);
-        } catch (UnsupportedComponentException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void initialize() {
+public class SearchRequestPanel extends RequestPanel {
+    protected void initialize() {
         setName("SEARCH REQUESTS");
         setBackground(Color.CYAN);
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        for (JComponent component : requests) add(component);
-    }
-
-    @Override
-    public String getData() {
-        StringBuilder builder = new StringBuilder();
-        for (JComponent component : requests) {
-            builder.append(((JLabel) component).getText());
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
-    @Override
-    public void setData(String data) {
-        JLabel label = new JLabel(data);
-        requests.add(label);
-        add(label);
-        revalidate();
     }
 }
