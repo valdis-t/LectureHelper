f4cfa445d90031b5920f96902265c5e8839040ac
valdis-v <vladislavkharkov95@gmail.com>
2022-03-18 13:11:25 +0200

    Make Start more simple for now

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,12 +1,7 @@
-import controller.TextController;
 import gui.MainFrame;
-
-import javax.swing.*;
 
 public class Start {
     public static void main(String[] args) {
-        JFrame frame = new MainFrame();
-        if (TextController.getController().isInitialised()) frame.setVisible(true);
-        else throw new RuntimeException();
+        new MainFrame().setVisible(true);
     }
 }
