5e63db43e45f45203337ab129885e2548bc328da
valdis-v <vladislavkharkov95@gmail.com>
2022-03-18 04:43:28 +0200

    Create abstract RequestPanel for containing the requests

---
+++ src/main/java/gui/panel/RequestPanel.java
@@ -0,0 +1,67 @@
+package gui.panel;
+
+import controller.Controller;
+import exception.UnsupportedComponentException;
+import interfaces.Changeable;
+
+import javax.swing.*;
+import java.util.Stack;
+
+public abstract class RequestPanel extends JPanel implements Changeable<String> {
+    private final Stack<JComponent> requests;
+
+    {
+        requests = new Stack<>();
+    }
+
+    public RequestPanel() {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        register();
+        initialize();
+    }
+
+    private void register() {
+        try {
+            Controller.register(this);
+        } catch (UnsupportedComponentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeLast() {
+        if (!requests.empty()) {
+            remove(requests.pop());
+        }
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public void removeAll() {
+        requests.forEach(this::remove);
+        requests.clear();
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public String getData() {
+        StringBuilder builder = new StringBuilder();
+        for (JComponent component : requests) {
+            builder.append(((JLabel) component).getText());
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public void setData(String data) {
+        JLabel label = new JLabel(data);
+        requests.add(label);
+        add(label);
+        revalidate();
+    }
+
+    protected abstract void initialize();
+}
