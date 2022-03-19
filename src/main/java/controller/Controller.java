528f974dfff60a0fbed4716873b8891264539103
valdis-v <vladislavkharkov95@gmail.com>
2022-03-20 01:03:28 +0200

    Modify Controller by implementing ActionListener delegate calling methods to actionPerformed make all methods as non-static

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,7 @@
 package controller;
 
+import static boot.GUIComponentEvent.*;
+
 import controller.component.ComponentController;
 import controller.file.FileController;
 import controller.text.DataController;
@@ -8,7 +10,10 @@ import interfaces.Changeable;
 import interfaces.Readable;
 import interfaces.entity.gui.CurrentRequest;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
     private final static ComponentController componentController = new ComponentController();
     private final static DataController dataController = new DataController();
     private final static FileController fileController = new FileController();
@@ -18,37 +23,61 @@ public class Controller {
         if (entry instanceof Changeable) componentController.register((Changeable<String>) entry);
     }
 
-    public static void addRequestToKeywords() {
+    private void addRequestToKeywords() {
         componentController.addRequestToKeywords();
     }
 
-    public static void addRequestToSearchRequests() {
+    private void addRequestToSearchRequests() {
         componentController.addRequestToSearchRequests();
     }
 
-    public static void removeLastKeyword() {
+    private void removeLastKeyword() {
         componentController.removeLastKeyword();
     }
 
-    public static void removeLastSearchRequest() {
+    private void removeLastSearchRequest() {
         componentController.removeLastSearchRequest();
     }
 
-    public static void removeAllKeywords() {
+    private void removeAllKeywords() {
         componentController.removeAllKeywords();
     }
 
-    public static void removeAllSearchRequests() {
+    private void removeAllSearchRequests() {
         componentController.removeAllSearchRequest();
     }
 
-    public static void cleanRequestField() {
+    private void cleanRequestField() {
         componentController.cleanRequestField();
     }
 
-    public static void printToCL(){ System.out.println(dataController.getWebPage()); }
+    private void printToCL(){ System.out.println(dataController.getWebPage()); }
 
-    public static void saveFile(){
+    private void saveFile(){
         fileController.saveFileAsHtml(dataController.getWebPage());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case ADD_TO_SEARCH_REQUESTS: addRequestToSearchRequests();
+            break;
+            case ADD_TO_KEYWORDS: addRequestToKeywords();
+            break;
+            case REMOVE_LAST_SEARCH_REQUEST: removeLastSearchRequest();
+            break;
+            case REMOVE_LAST_KEYWORD: removeLastKeyword();
+            break;
+            case SAVE: saveFile();
+            break;
+            case REMOVE_ALL_KEYWORDS: removeAllKeywords();
+            break;
+            case REMOVE_ALL_SEARCH_REQUESTS: removeAllSearchRequests();
+            break;
+            case CLEAN_REQUEST_FIELD: cleanRequestField();
+            break;
+            case PRINT_TO_CL: printToCL();
+            break;
+        }
     }
 }
